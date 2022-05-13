c8c076eee346d473224e9a5d2f377fce47ce0bb6
Pawel Filipkowski <pawebf1@gmail.com>
2022-05-13 12:26:17 +0200

    add phpmailer and phpdotenv

--- src/views/include/header.php
+++ src/views/include/header.php
@@ -6,5 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
           integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous'>
-    <title>Sam php</title>
+    <?php
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__,3));
+    $dotenv->load();
+    echo "<title>" . $_ENV['COMPOSE_PROJECT_NAME'] . "</title>";
+    ?>
 </head>
+<body>
